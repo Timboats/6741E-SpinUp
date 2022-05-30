c971bf7f6d39f1c1848410ffc1bd9e50924dcb09
Timboats <72775377+Timboats@users.noreply.github.com>
2022-05-30 19:07:15 -0400

    Added faceHeading method to drivetrain

--- include/drivetrain.h
+++ include/drivetrain.h
@@ -24,6 +24,7 @@ public:
 Drivetrain(float wheelDiameter, float gearRatio, vex::motor northMotor, vex::motor southMotor, vex::motor eastMotor, vex::motor westMotor, int northWheelAngle, int southWheelAngle, int eastWheelAngle, int westWheelAngle, vex::gps gps1, vex::gps gps2 = NULL);
 void goToPos(int x, int y);
 void stopAllDrive();
+void faceHeading(int heading);
   
 
 
--- src/drivetrain.cpp
+++ src/drivetrain.cpp
@@ -81,3 +81,43 @@ void Drivetrain::goToPos(int x, int y){
   
     
 }
+void Drivetrain::faceHeading(int heading){
+    const float Kp = 250;
+    const float Ki = 0;
+    const float Kd = 0;
+
+    float deltaTime = 0;
+    float prevTime = 0;
+    int error = 0;
+
+    int angleFromSet = 0; //Difference between current heading and desired heading from 0-360
+    float totalVoltage = 0;
+
+    while(true){
+        angleFromSet = ((Simpler::degreeToStdPos(gps1.heading()) - heading) + 360) % 360;
+
+        if(angleFromSet > 180){
+            error = -(180 - (angleFromSet - 180));
+        } else {
+            error = angleFromSet;
+        }
+
+        totalVoltage = Kp * error;
+
+        northMotor.spin(forward, totalVoltage, vex::voltageUnits::mV);
+        eastMotor.spin(forward, totalVoltage, vex::voltageUnits::mV);
+        southMotor.spin(forward, totalVoltage, vex::voltageUnits::mV);
+        westMotor.spin(forward, totalVoltage, vex::voltageUnits::mV);
+
+        if(Simpler::abs(error) <= 1){
+            deltaTime = Brain.Timer.time(msec) - prevTime;
+            if(deltaTime > 500){
+                stopAllDrive;
+                return;
+            }
+        }
+        else {
+            prevTime = Brain.Timer.time(msec);
+        }
+    }
+}
