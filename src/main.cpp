2610d34103173cc0fbaa5f53494c271aabdf150a
Timboats <72775377+Timboats@users.noreply.github.com>
2022-08-31 14:29:59 -0400

    Update main.cpp
    -Removed some unnescary stuffs not needed for the launcher
    -setup launcher motors and voltage variables
    -added voltage control for left and right launcher motors to the controller

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include "mathlib.h"
 #include "pros/gps.hpp"
 #include "pros/llemu.hpp"
+#include "pros/misc.h"
 #include "pros/misc.hpp"
+#include "pros/motors.h"
 #include "pros/motors.hpp"
 #include "pros/rtos.hpp"
 #include <cstddef>
@@ -16,12 +18,43 @@
 #define EASTMOTORPORT 20
 #define WESTMOTORPORT 1
 #define GPS1PORT 15
+#define launcherMotorLeftPort 18 //POV from entrance/intake of launcher
+#define launcherMotorRightPort 19
 
 bool buttonAState = 0;
+float leftMotorVoltage = 0;
+float rightMotorVoltage = 0;
+
 pros::Controller master = pros::Controller(pros::E_CONTROLLER_MASTER);
 Drivetrain train(3.25, 1, NORTHMOTORPORT, SOUTHMOTORPORT, EASTMOTORPORT, WESTMOTORPORT, 45, 225, 315, 135, GPS1PORT);
 
-//TODO button callbacks still might not work problem-digital_new could be helpful tho
+void controllerInput(){
+  if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN) == true){
+    if(leftMotorVoltage != -12){
+      leftMotorVoltage-=0.5;
+      master.print(0, 0, "Left: %f", leftMotorVoltage);
+    }
+  }
+  if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP) == true){
+    if(leftMotorVoltage != 12){
+      leftMotorVoltage+=0.5;
+      master.print(0, 0, "Left: %f", leftMotorVoltage);
+    }
+  }
+  if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B) == true){
+    if(rightMotorVoltage != -12){
+      rightMotorVoltage-=0.5;
+      master.print(1, 0, "Right: %f", rightMotorVoltage);
+    }
+  }
+  if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X) == true){
+    if(rightMotorVoltage != 12){
+      rightMotorVoltage+=0.5;
+      master.print(1, 0, "Right: %f", rightMotorVoltage);
+
+    }
+  }
+}
 
 void toggleButtonA(){
   if (buttonAState == 0){
@@ -43,6 +76,11 @@ void initialize() {
   pros::Motor SouthMotorInit(SOUTHMOTORPORT, pros::E_MOTOR_GEARSET_18, true);
   pros::Motor WestMotorInit(WESTMOTORPORT, pros::E_MOTOR_GEARSET_18, true);
   pros::Gps GpsPrimaryInit(GPS1PORT, 0.00, -0.03);
+
+  pros::Motor launcherMotorLeft(launcherMotorLeftPort, pros::E_MOTOR_GEARSET_06, true);
+  pros::Motor launcherMotorRight(launcherMotorRightPort, pros::E_MOTOR_GEARSET_06);
+
+  master.clear();
   
 
   train = Drivetrain(3.25, 1, NORTHMOTORPORT, SOUTHMOTORPORT, EASTMOTORPORT, WESTMOTORPORT, 45, 225, 135, 315, GPS1PORT);
@@ -52,155 +90,27 @@ void initialize() {
 
 }
 
-int errorTTP = 0;
-int prevErrorTTP = 0;
-//There is no loop inside the turnTowardsPoint function
-//So, these variables needed to be global
-//They have TTP at the end to indicate this (Turn Towards Point)
-
-float turnTowardsPoint(int x, int y){
-  pros::Gps GpsPrimary(GPS1PORT);
-  float motorPercentage = 0;
-  const float Kp = 1;
-  const float Ki = 0.01;
-  const float Kd = 0.3;
-  const int windupUpperLimit = 15;
-  float integral = 0;
-  int angleFromDesired = 0;
-  int desiredAngle = 0;
-
-  desiredAngle = atan2(y - (GpsPrimary.get_status().y * 1000), x - (GpsPrimary.get_status().x * 1000)) * (180/M_PI); //in mm prolly gonna break
-
-  angleFromDesired = (((desiredAngle - Simpler::degreeToStdPos(GpsPrimary.get_heading())) + 360) % 360);
-  //finds the difference in the current angle and the desired angle from 0 to 360 degrees
-
-  if (angleFromDesired > 180){
-    errorTTP = -(180 - (angleFromDesired - 180));
-  } else {
-    errorTTP = angleFromDesired;
-  }
-  //translates angleFromDesired to -180 to 180 degrees
-
-  integral = integral + errorTTP;
-  //Adds up the area of the error versus time graph from a specified time until the current time
-
-  if (errorTTP > windupUpperLimit){
-    integral = 0;
-  }
-  //Defines the lower limit of the integral
-
-  motorPercentage = -(Kp * errorTTP) + (Ki*integral) + (Kd*(errorTTP - prevErrorTTP));
-  //calculates the desired voltage of the motors
-
-  prevErrorTTP = errorTTP; 
-  //Used to calculate integral
-    
-  return(motorPercentage);
-  //Value is fed into motor to facilitate desired action
-}
-
-void testGoToMethod(){
-  pros::Gps GpsPrimary(GPS1PORT);
-
-  int randomAngle = 0;
-  int xRandom = 0;
-  int yRandom = 0; 
-
-  srand((unsigned) pros::millis());
-  randomAngle = (rand() % 360) + 1;
-  train.faceHeading(randomAngle);
-  //This is to prove that the goTo method works no matter the heading of the robot
-
-  srand((unsigned) pros::millis());
-  xRandom = (rand() % 1601) - 800;
-
-  pros::delay((rand() % 3 + 1)*1000);
-
-  srand((unsigned) pros::millis());
-  yRandom = (rand() % 1601) - 800;
-  //generates a set of random coordinates
-
-  master.clear();
-  std::cout << "Dx: "+std::to_string(xRandom) << std::endl;
-  std::cout << "Dy: "+std::to_string(yRandom) << std::endl;
 
 
-  //This prints the desired position, that was randomly generated, on the screen of the controller
-
-  train.goToPos(xRandom, yRandom);
-  //This sends the robot to a random x and y position
-
-
-  std::cout << "Ax: "+std::to_string(GpsPrimary.get_status().x * 1000) << std::endl;
-
-  std::cout << "Ay: "+std::to_string(GpsPrimary.get_status().y * 1000) << std::endl;
-
-
-  //This prints where the robot actually went on the screen of the controller
-
-  return;
-}
-
-
-
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
 void disabled() {}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
+
 void competition_initialize() {}
 
 
 void autonomous() {
-  pros::Gps GpsPrimary(GPS1PORT);
-
-
-  // train.goToPos(0, 0, master);
-  testGoToMethod();
-  
-  // master.set_text(1, 1, std::to_string(GpsPrimary.get_heading()));
-  // pros::delay(100);
 
 }
 
 
 void opcontrol() {
-  // train.goToPos(0, 0);
-  // testGoToMethod();
-	// int storedPercentage = 0;
-
-  settings blank;
-  blank.goToPos_kp = 92;
-  blank.goToPos_ki = 45;
-  writeSettings(blank);
-  // getSettings();
-  
 	while (true) {
-    // if(master.get_digital(pros::E_CONTROLLER_DIGITAL_A) == true){
-    //   toggleButtonA();
-    // }
+    controllerInput();
+    pros::Motor launcherMotorLeft(launcherMotorLeftPort);
+    pros::Motor launcherMotorRight(launcherMotorRightPort);
 
-		// if (buttonAState == 1){
-		// 	storedPercentage = turnTowardsPoint(0, 0);
-		// } 
-		// else {
-		// 	storedPercentage = 0;
-		// }
-    // //This will allow the robot to turn to face a goal while still being able to be driven around
-
-    // train.steeringControl(master, storedPercentage);
-    // //This allows for driver control. By modifying the value outputted by the control stick, the movement of the robot is relative to the field, rather than the heading.
-		pros::delay(20);
+    launcherMotorLeft.move_voltage(leftMotorVoltage*1000);
+    launcherMotorRight.move_voltage(rightMotorVoltage*1000);
+	  pros::delay(20);
 	}
 }
